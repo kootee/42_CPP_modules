cb8bc67ea132a166ffb7656624e6965fc1abfd36
kootee <katjatoivola@icloud.com>
2024-10-06 16:08:54 +0300

    fixed bug in check search index function

--- module00/ex01/phonebook.cpp
+++ module00/ex01/phonebook.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:01:10 by ktoivola          #+#    #+#             */
-/*   Updated: 2024/10/06 14:00:23 by ktoivola         ###   ########.fr       */
+/*   Updated: 2024/10/06 16:07:53 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int exit_phonebook(int exit_value)
     return (0);
 }
 
-int check_search_id(const string& str)
+int check_search_id(const string& str, int phonebook_id)
 {
     int id;
-    
+
     string::const_iterator c = str.begin();
     while (c != str.end())
     {
@@ -34,11 +34,13 @@ int check_search_id(const string& str)
         c++;   
     }
     id = atoi(str.c_str());
-	if (id > 7 || id < 0)
-		cout << OUT_OF_RANGE_ID << endl;
-    else if (!index || id > (((int)index - 1) % 8))
-        cout << OUT_OF_RANGE_ID << endl;
-    return (0);
+	if ((id > 7 || id < 0) 
+        || (phonebook_id == 0 || id > (phonebook_id - 1) % 8))
+        {
+		    cout << OUT_OF_RANGE_ID << endl;
+            return (-1);
+        }
+    return (id);
 }
 
 int PhoneBook:: index = 0, status = 0;
@@ -59,7 +61,6 @@ void	PhoneBook::add_contact()
         return ;
 	contacts[index % 8] = Contact (name, lastname, nickname, number, secret);
 	index++;
-    cout << "index is " << index << endl;
 }
 
 void	PhoneBook::search_contact()
@@ -78,8 +79,8 @@ void	PhoneBook::search_contact()
             return ;
         if (input == "BACK")
             return ;
-        id_print = check_search_id(input);
-        if (id_print == -1)
+        id_print = check_search_id(input, this->index);
+        if (id_print != -1)
 		{
 			contacts[id_print].print_contact_info(id_print);
 			return ;
