502975a83e250ef5a5a9843374f0e4da625e0688
kootee <katjatoivola@icloud.com>
2025-01-19 15:49:19 +0200

    adding module 07

---
+++ module07/ex00/MyTemplates.hpp
@@ -0,0 +1,14 @@
+template <typename T> T swap(T x, T y)
+{
+
+}
+
+template <typename T> T min(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+template <typename T> T max(T x, T y) 
+{
+    return (x < y) ? x : y;
+}
